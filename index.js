6f39a24a2290a179b7b36550c40c592daef95f29
Mark Meyer <mark@photo-mark.com>
2018-04-01 12:38:16 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+/**
+ * Simple pure javascript heap implimentation
+ * Storage is a simple array
+ * Supports insert and pop for both min and max heaps
+ * 
+ * Pass in optional value_function to control how objects are sorted
+ * value_function should take a single argument and return a value which
+ * will be used in comparisons
+ * 
+ * Mark Meyer | mark@photo-mark.com
+ * 
+ */
+
+class Heap{
+    constructor(cmp){
+        this._s = []
+        this._comp = cmp || ((a, b) => a < b)
+    }
+    static fromArray(arr, comp = ((a, b) => a < b)) {
+        /* from Skiena */
+        let h = new Heap(comp)
+        h._s = arr
+        for (let i = arr.length; i >= 0; i--){
+            h.bubbledown(i)
+        }
+        return h
+    }
+    get length(){
+         return this._s.length
+    }
+    *[Symbol.iterator](){
+        /* Allows iteration over heap 
+           i.e. Array.from(heap) or for (n of heap){}
+           THIS IS DESTRUCTIVE. It works by removing elements 
+           off the top of the heap.
+        */
+        let v = this.pop()
+        while (v){
+            yield v
+            v = this.pop()
+        }     
+    }
+    insert(object){
+        this._s.push(object)
+        this.bubbleup(this.length -1)
+    }
+    peek(){
+        return this._s[0]
+    }
+    pop(){
+        if (this.length <= 1) {
+            return this._s.pop() // last item or undefined if empty
+        }
+        let item = this._s[0]
+        this._s[0] = this._s.pop()
+        this.bubbledown(0)
+        return item
+    }
+    bubbleup(index){
+        if(index <= 0) return // at top
+        let parent_index = Math.floor((index-1)/2)
+       
+        if (this._comp(this._s[index], this._s[parent_index])){
+            [this._s[parent_index], this._s[index]] = [this._s[index],this._s[parent_index] ]
+            this.bubbleup(parent_index)
+        }
+    }
+    bubbledown(index){
+        let c = index * 2 + 1 /* child index */
+        let swapIndex = index
+ 
+        for (let i = 0; i <= 1; i++){
+            if(c+i < this.length && this._comp(this._s[c+i], this._s[swapIndex])){
+                    swapIndex = c+i
+            }
+        }
+        if (swapIndex != index){
+            [this._s[swapIndex], this._s[index]] = [this._s[index],this._s[swapIndex] ]
+            this.bubbledown(swapIndex)
+        }    
+     }
+ }
+
+module.exports = Heap
---
+++ test/index.js
@@ -0,0 +1,63 @@
+const Heap = require('../index')
+const assert = require('assert')
+let h = new Heap()
+
+describe("Without a compare function", function(){
+    it("first element is smallest item", function(){
+        let h = new Heap()
+        let arr = [8, 4, 2, 3, 7, 1, 9, 5, 6]
+        arr.forEach(n => h.insert(n))
+        let n = h.peek()
+        assert.equal(n, 1)
+    })
+    it("items are popped in sorted descending order", function(){
+        let h = new Heap()
+        let arr = [8, 4, 2, 3, 7, 1, 9, 5, 6]
+        arr.forEach(n => h.insert(n))
+        let n = h.pop()
+        let l = h.pop()
+        do {
+            assert(l > n);
+            [n, l] = [l, h.pop()]
+        } while (l)
+    })
+    it("iterator produces ordered items", function(){
+        let h = new Heap()
+        let arr = [8, 4, 2, 3, 7, 1, 9, 5, 6]
+        arr.forEach(n => h.insert(n))
+        let sorted = Array.from(h)
+        assert.deepStrictEqual(sorted, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+    })
+})
+describe("With a compare function", function(){
+    it("first element is determined by compare", function(){
+        let h = new Heap((a, b) => a.value > b.value)
+        let arr = [{value: 2}, {value: 1}, {value: 7}, {value: 3}]
+        arr.forEach(n => h.insert(n))
+        let n = h.peek()
+        assert.equal(n.value, 7)
+    })
+    it("items are popped in ascending order when comp function is opposite default", function(){
+        let h = new Heap((a, b) => a > b)
+        let arr = [8, 4, 2, 3, 7, 1, 9, 5, 6]
+        arr.forEach(n => h.insert(n))
+        let n = h.pop()
+        let l = h.pop()
+        do {
+            assert(l < n);
+            [n, l] = [l, h.pop()]
+        } while (l)
+    })
+})
+describe("fromArray factory", function(){
+    it("produces heap from array", function(){
+        let arr = [7, 2, 4, 8, 1, 9, 3, 6, 5]
+        let h = Heap.fromArray(arr)
+        assert.deepStrictEqual(Array.from(h), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+    })
+    it("produces heap from array with comparison function", function(){
+        let arr = [7, 2, 4, 8, 1, 9, 3, 6, 5]
+        let h = Heap.fromArray(arr, (a, b) => a > b)
+        assert.deepStrictEqual(Array.from(h), [9, 8, 7, 6, 5, 4, 3, 2, 1])
+    })
+})
